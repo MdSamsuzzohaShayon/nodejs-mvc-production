b675ad8e3fd91ad2514d7c7cefa8b301305abf9e
Md Shayon <mdshayon0@gmail.com>
2020-06-15 22:32:02 +0600

    finished author routes show and update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 
 const indexRouter = require('./routes/index');
@@ -28,6 +29,7 @@ app.set('view engine', "ejs");
 app.set('views', __dirname+'/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
